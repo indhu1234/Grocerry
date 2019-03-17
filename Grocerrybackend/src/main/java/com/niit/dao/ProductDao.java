a5773c697710bd617ef7f839e7a1f7e2c4f632a0
MR USER <MR USER@DESKTOP-9UC000P>
2019-03-17 21:48:45 +0530

    Commits dao

---
+++ Grocerrybackend/src/main/java/com/niit/dao/ProductDao.java
@@ -0,0 +1,25 @@
+package com.niit.dao;
+
+import java.util.List;
+
+import com.niit.model.Category;
+import com.niit.model.Product;
+
+
+public interface ProductDao {
+	
+
+	List<Product> getAllProducts(); //select * from product
+	Product getProduct(int id);
+	void deleteProduct(int id); //delete from product where id=?
+	List<Category> getCategories();
+	void saveOrUpdateProduct(Product product);
+
+	
+	
+}
+
+
+
+
+
